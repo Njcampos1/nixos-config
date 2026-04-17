e2ea0c12bbbca6d7b847f5b2309315e66597536e
njcampos1 <njcampos1@uc.cl>
2026-04-17 00:36:41 -0400

    fix: move configuration SSH

--- modules/home/ssh.nix
+++ modules/home/ssh.nix
@@ -1,27 +1,18 @@
 # /etc/nixos/modules/home/ssh.nix
 { config, pkgs, ... }:
 
-let
-  secrets = if builtins.pathExists ./azure-ip.nix 
-            then import ./azure-ip.nix 
-            else { azure_ip = "0.0.0.0"; }; 
-in
-
 {
   programs.ssh = {
     enable = true;
     enableDefaultConfig = false;
+    
+    includes = [ "~/.ssh/config_azure" ];
+
     matchBlocks = {
       "github.com" = {
         hostname = "github.com";
         user = "git";
         identityFile = "~/.ssh/id_ed25519";
-        identitiesOnly = true;
-      };
-      "azure-server" = {
-        hostname = secrets.azure_ip;
-        user = "azureuser";
-        identityFile = "~/.ssh/keys/upper-key.pem";
         identitiesOnly = true;
       };
     };
